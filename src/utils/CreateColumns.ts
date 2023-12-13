6e2c98c7dc34b5421aaed865b8a4f29bd9982cba
Roberto Loss <robertoloss@gmail.com>
2023-12-13 18:52:13 -0500

    fixes

--- src/utils/CreateColumns.ts
+++ src/utils/CreateColumns.ts
@@ -1,33 +1,34 @@
 import { urlFor } from "./sanity-queries";
+import { Section } from "@/sanity/sanity-types";
 
 export type ColumnType = {
 	title: string, 
-	content: any[], 
-	iconUrl: string, 
+	content: string[], 
+	iconUrl: string | undefined, 
 	figure: string,
 	iconsYes: boolean
 }
 
 
-export function createColumns( item : any ) : any {
+export function createColumns( item : Section["three_cols_in_section"] ) : ColumnType[] {
 	
-	const threeCols = item?.three_cols_proj === undefined ? {...item} : {...item.three_cols_proj};
+	const threeCols = item // === undefined ? {...item} : {...item.three_cols_proj};
 
-	const colTitles = threeCols.columns_titles ? [ ...threeCols.columns_titles ] : []
-	const colIcons = threeCols.icons ? [ ...threeCols.icons ] : []
-	const colFigures = threeCols.figures ? [ ...threeCols.figures ] : []
-	const colContents = [threeCols.columns_text_1, threeCols.columns_text_2, threeCols.columns_text_3]
+	const colTitles = threeCols!.columns_titles ? [ ...threeCols!.columns_titles ] : []
+	const colIcons = threeCols!.icons ? [ ...threeCols!.icons ] : []
+	const colFigures = threeCols!.figures ? [ ...threeCols!.figures ] : []
+	//const colContents = [threeCols!.columns_text_1, threeCols!.columns_text_2, threeCols!.columns_text_3]
 
   const columns : ColumnType[] = [];
 
 	for (let i=0; i<3; i++) {
-		const column : ColumnType = { title: "", content: [], iconUrl: "", figure: "", iconsYes: threeCols.are_there_icons };
+		const column : ColumnType = { title: "", content: [], iconUrl: "", figure: "", iconsYes: threeCols!.are_there_icons || false};
 		column.title = colTitles[i];
-		column.content = colContents[i]
+		// column.content = colContents!
 		column.iconUrl = colIcons[i]?.image ? urlFor(colIcons[i]?.image)?.width(28)?.url() : ""
 		column.figure = colFigures[i]
 		columns.push(column)
- 	}
+	}
 
 	return columns
 }
